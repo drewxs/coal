c114e630358137c6cc66c0c5cead496b40726b07
Andrew X. Shah <drewshah0@gmail.com>
2024-09-18 00:47:46 -0600

    feat(main): read tokens from file
    for now

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::io::Read;
+
+use coal::lexer::Lexer;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    std::io::stdin().read_to_string(&mut input).unwrap();
+
+    let mut lexer = Lexer::new(&input);
+
+    loop {
+        println!("{}", lexer.next_token());
+    }
 }
