ff08fbbda82ff579101fd8b9af8e753d76e92a86
Andrew X. Shah <drewshah0@gmail.com>
2024-09-18 02:10:15 -0600

    feat: create repl base

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,25 @@
-use std::io::Read;
+use std::{env, fs::File, io::Read};
 
-use coal::lexer::Lexer;
+use coal::{lexer::Lexer, repl::repl, token::Token};
 
 fn main() {
-    let mut input = String::new();
-    std::io::stdin().read_to_string(&mut input).unwrap();
+    let args: Vec<String> = env::args().collect();
+    match args.len() {
+        2 => run(&args[1]),
+        1 => repl(),
+        _ => println!("Usage: coal [file?]"),
+    }
+}
 
+fn run(filename: &str) {
+    let mut file = File::open(filename).unwrap();
+    let mut input = String::new();
+    file.read_to_string(&mut input).unwrap();
     let mut lexer = Lexer::new(&input);
 
-    loop {
-        println!("{}", lexer.next_token());
+    let mut token = lexer.next_token();
+    while token != Token::EOF {
+        println!("{token}");
+        token = lexer.next_token();
     }
 }
---
+++ src/repl.rs
@@ -0,0 +1,25 @@
+use std::io::{self, Write};
+
+use crate::lexer::Lexer;
+
+pub fn repl() {
+    println!("Coal 0.1.0");
+    loop {
+        print!(">> ");
+        io::stdout().flush().unwrap();
+
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+
+        if input.trim() == "exit" {
+            break;
+        }
+
+        let lexer = Lexer::new(&input);
+
+        for token in lexer {
+            println!("{token:?}");
+            io::stdout().flush().unwrap();
+        }
+    }
+}
