d6317da497e4e182964a7bd2f7f7972ca30f41c6
Andrew X. Shah <drewshah0@gmail.com>
2024-09-29 03:49:31 -0600

    feat(repl): parse/check input

--- src/repl.rs
+++ src/repl.rs
@@ -1,6 +1,6 @@
 use std::io::{self, Write};
 
-use crate::{config::VERSION, Lexer};
+use crate::{config::VERSION, Lexer, Parser};
 
 pub fn repl() {
     println!("Coal {VERSION}");
@@ -11,15 +11,19 @@ pub fn repl() {
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
 
-        if input.trim() == "exit" {
-            break;
+        match input.trim() {
+            "exit" | "quit" => break,
+            _ => {}
         }
 
         let lexer = Lexer::new(&input);
+        let mut parser = Parser::new(lexer.clone());
+        parser.parse();
+        parser.check();
 
-        for token in lexer {
-            println!("{token}");
-            io::stdout().flush().unwrap();
-        }
+        // for token in lexer {
+        //     println!("{token}");
+        //     io::stdout().flush().unwrap();
+        // }
     }
 }
