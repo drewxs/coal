1f7c61c17abf3b0a558c46c9467b3a5857d2208b
Andrew X. Shah <drew@drewxs.dev>
2024-11-29 20:27:01 -0700

    refactor(fmt): cleanup

--- coal/src/commands/fmt.rs
+++ coal/src/commands/fmt.rs
@@ -1,8 +1,8 @@
 use std::{fs, time::Instant};
 
-use ignore::{DirEntry, Walk};
-
 use coal_core::Program;
+
+use crate::path::program_files;
 
 pub fn fmt(input: &str) -> String {
     Program::from(input).to_string()
@@ -36,27 +36,19 @@ pub fn fmt_file_dry_run(path: &str) -> Result<String, String> {
 pub fn fmt_path(path: &str, dry_run: bool) -> Result<String, String> {
     let mut out = String::new();
 
-    for entry in iter_src_files(path) {
-        let path = entry.path();
-        let file_path = path.to_str().unwrap();
+    for entry in program_files(path) {
+        let path = entry.path().to_str().expect("invalid utf-8");
 
         if dry_run {
-            let res = fmt_file_dry_run(file_path)?;
+            let res = fmt_file_dry_run(path)?;
             out.push_str(&res);
         } else {
-            let ms = fmt_file(file_path)?;
-            out.push_str(&format!("{file_path} {ms}ms\n"));
+            let ms = fmt_file(path)?;
+            out.push_str(&format!("{path} {ms}ms\n"));
         }
     }
 
     Ok(out)
-}
-
-fn iter_src_files(path: &str) -> impl Iterator<Item = DirEntry> {
-    Walk::new(path).flatten().filter(|e| {
-        e.metadata().ok().is_some_and(|m| m.is_file())
-            && e.path().extension().is_some_and(|ext| ext == "coal")
-    })
 }
 
 #[cfg(test)]
@@ -73,20 +65,21 @@ mod tests {
 
     #[test]
     fn test_fmt_nested_ifs() {
-        let input = r#"
-      let       x =        5   ;
-   let y   = 10  ;
+        let input = "
+          let       x =        5   ;
+       let y   = 10  ;
 
-           if x  > y {
-    if x > 1 {        let z = 1.;
-            return z;  } else {
-        return 1;  }   } elif x < y {
-              return y; } else {
-                        return   0;
-                     }
-        "#;
-        let expected = r#"let x: int = 5;
+               if x  > y {
+        if x > 1 {        let z = 1.;
+                return z;  } else {
+            return 1;  }   } elif x < y {
+                  return y; } else {
+                            return   0;
+                         }
+            ";
+        let expected = "let x: int = 5;
 let y: int = 10;
+
 if x > y {
     if x > 1 {
         let z: float = 1.0;
@@ -99,7 +92,7 @@ if x > y {
 } else {
     return 0;
 }
-"#;
+";
 
         assert_eq!(expected, fmt(input));
     }
--- coal/src/path.rs
+++ coal/src/path.rs
@@ -3,15 +3,7 @@ use std::{
     path::{Path, PathBuf},
 };
 
-pub fn main_program(path: Option<String>) -> Result<String, String> {
-    path.or_else(|| {
-        ["main.coal", "src/main.coal"]
-            .iter()
-            .find(|&p| Path::new(p).exists())
-            .map(|&p| p.to_string())
-    })
-    .ok_or(String::from("no 'main.coal' or 'src/main.coal' found"))
-}
+use ignore::{DirEntry, Walk};
 
 pub fn data() -> PathBuf {
     let data_dir = dirs::data_dir().unwrap_or_default();
@@ -29,6 +21,23 @@ pub fn cache() -> PathBuf {
 
 pub fn history() -> PathBuf {
     data().join("history")
+}
+
+pub fn main_program(path: Option<String>) -> Result<String, String> {
+    path.or_else(|| {
+        ["main.coal", "src/main.coal"]
+            .iter()
+            .find(|&p| Path::new(p).exists())
+            .map(|&p| p.to_string())
+    })
+    .ok_or(String::from("no 'main.coal' or 'src/main.coal' found"))
+}
+
+pub fn program_files(path: &str) -> impl Iterator<Item = DirEntry> {
+    Walk::new(path).flatten().filter(|e| {
+        e.metadata().ok().is_some_and(|m| m.is_file())
+            && e.path().extension().is_some_and(|ext| ext == "coal")
+    })
 }
 
 fn resolve_program_dir(path: &Path) -> PathBuf {
