2cb5f4acc483bcdaa4ef7e853ace3ae3a6fff5a7
Andrew X. Shah <drew@drewxs.dev>
2024-12-08 04:38:35 -0700

    fix(evaluator): improved err fmt for multiline inputs

--- coal-core/src/evaluator/mod.rs
+++ coal-core/src/evaluator/mod.rs
@@ -48,12 +48,22 @@ impl Evaluator {
                 Object::Error {
                     message,
                     span: ((l1, c1), (l2, c2)),
-                } if l1 == 1 && l2 == 1 => {
-                    println!(
-                        "\x1b[31m{}\x1b[0m\n{}",
-                        " ".repeat(c1 + 2) + &"^".repeat(c2 - c1),
-                        message
-                    );
+                } => {
+                    if input.lines().count() > 1 {
+                        let line = input.lines().nth(l1 - 1).unwrap();
+                        println!("\n{line}");
+                        println!(
+                            "\x1b[31m{}\x1b[0m",
+                            " ".repeat(c1 - 1) + &"^".repeat(c2 - c1),
+                        );
+                        println!("[{l1}:{c1}-{l2}:{c2}] {message}");
+                    } else {
+                        println!(
+                            "\x1b[31m{}\x1b[0m\n",
+                            " ".repeat(c1 + 2) + &"^".repeat(c2 - c1),
+                        );
+                        println!("{message}");
+                    }
                 }
                 _ => println!("{obj}"),
             }
