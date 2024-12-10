cb176928a114cd28c44bdc08bc03a5dca69bb4e7
Andrew X. Shah <drew@drewxs.dev>
2024-12-10 02:17:16 -0700

    feat(types): type infer infix exprs

--- coal-core/src/ast/types.rs
+++ coal-core/src/ast/types.rs
@@ -1,6 +1,6 @@
 use std::{borrow::Borrow, fmt};
 
-use crate::{Object, Token};
+use crate::{Object, Span, Token};
 
 use super::{Expr, Ident, Infix, Literal, Stmt};
 
@@ -59,13 +59,7 @@ impl TryFrom<&Expr> for Type {
             Expr::Literal(Literal::Int(_), _) => Ok(Type::Int),
             Expr::Literal(Literal::Float(_), _) => Ok(Type::Float),
             Expr::Literal(Literal::Bool(_), _) => Ok(Type::Bool),
-            Expr::Infix(op, lhs, _, _) => {
-                if *op == Infix::Div {
-                    Ok(Type::Float)
-                } else {
-                    Type::try_from(Borrow::<Expr>::borrow(lhs))
-                }
-            }
+            Expr::Infix(op, lhs, rhs, span) => Ok(infer_infix_type(op, lhs, rhs, span)),
             Expr::Prefix(_, rhs, _) => Type::try_from(Borrow::<Expr>::borrow(rhs)),
             Expr::Ident(ident, _) => Ok(Type::UserDefined(ident.name())),
             Expr::Fn { ret_t, .. } => Ok(ret_t.clone()),
@@ -73,6 +67,46 @@ impl TryFrom<&Expr> for Type {
             _ => Err(String::from("invaild literal")),
         }
     }
+}
+
+fn infer_infix_type(op: &Infix, lhs: &Expr, rhs: &Expr, _span: &Span) -> Type {
+    if op == &Infix::Div {
+        return Type::Float;
+    }
+
+    let lhs_t = infer_expr_type(lhs);
+    let rhs_t = infer_expr_type(rhs);
+
+    if lhs_t == Type::Int && rhs_t == Type::Int {
+        return Type::Int;
+    }
+
+    Type::Float
+}
+
+fn infer_expr_type(expr: &Expr) -> Type {
+    match expr {
+        Expr::Infix(op, lhs, rhs, span) => {
+            let t = infer_infix_type(op, lhs, rhs, span);
+            if t == Type::Int {
+                return Type::Int;
+            }
+        }
+        Expr::Prefix(_, rhs, _) => match rhs.borrow() {
+            Expr::Infix(i_op, i_lhs, i_rhs, i_span) => {
+                let t = infer_infix_type(i_op, i_lhs, i_rhs, i_span);
+                if t == Type::Int {
+                    return Type::Int;
+                }
+            }
+            Expr::Literal(Literal::Int(_), _) => return Type::Int,
+            _ => {}
+        },
+        Expr::Literal(Literal::Int(_), _) => return Type::Int,
+        _ => {}
+    }
+
+    Type::Float
 }
 
 impl fmt::Display for Type {
