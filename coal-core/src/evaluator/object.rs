aa720da8badfd6bcd13e9091932260794a22543f
Andrew X. Shah <drew@drewxs.dev>
2024-12-16 01:13:18 -0700

    fix(evaluator/object): display fn hash, rm display indentation

--- coal-core/src/evaluator/object.rs
+++ coal-core/src/evaluator/object.rs
@@ -2,7 +2,7 @@ use std::{
     cell::RefCell,
     collections::HashMap,
     fmt,
-    hash::{Hash, Hasher},
+    hash::{DefaultHasher, Hash, Hasher},
     rc::Rc,
 };
 
@@ -46,69 +46,15 @@ impl Object {
     pub fn is_truthy(&self) -> bool {
         !matches!(self, Object::Bool(false) | Object::Nil)
     }
+
+    pub fn calculate_hash(&self) -> u64 {
+        let mut hasher = DefaultHasher::new();
+        self.hash(&mut hasher);
+        hasher.finish()
+    }
 }
 
 impl Eq for Object {}
-
-impl Hash for Object {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        match self {
-            Object::Int(i) => i.hash(state),
-            Object::Float(f) => f.to_bits().hash(state),
-            Object::Str(s) => s.hash(state),
-            Object::Bool(b) => b.hash(state),
-            _ => "".hash(state),
-        }
-    }
-}
-
-impl Object {
-    pub fn fmt_with_indent(&self, f: &mut fmt::Formatter, indent_level: usize) -> fmt::Result {
-        let indent = "    ".repeat(indent_level);
-
-        match self {
-            Object::Int(i) => write!(f, "{i}"),
-            Object::Float(x) => write!(f, "{x:?}"),
-            Object::Str(s) => write!(f, "{s}"),
-            Object::Bool(b) => write!(f, "{b}"),
-            Object::List { data, .. } => write!(f, "{data:?}"),
-            Object::Map { data, .. } => write!(f, "{data:?}"),
-            Object::Fn {
-                name,
-                args,
-                body,
-                ret_t: t,
-                ..
-            } => {
-                let args_str = args
-                    .iter()
-                    .map(|arg| format!("{arg}"))
-                    .collect::<Vec<String>>()
-                    .join(", ");
-                write!(f, "{name}({args_str}) -> {t} {{")?;
-                for stmt in body {
-                    writeln!(f, "{indent}    {stmt}")?;
-                }
-                write!(f, "}}")
-            }
-            Object::Nil => write!(f, "nil"),
-            Object::Return(v) => write!(f, "{v}"),
-            Object::Error {
-                message,
-                span: trace,
-            } => {
-                let ((l1, c1), (l2, c2)) = trace;
-                write!(f, "{l1}:{c1}-{l2}:{c2} {}", message)
-            }
-        }
-    }
-}
-
-impl fmt::Display for Object {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        self.fmt_with_indent(f, 0)
-    }
-}
 
 impl From<bool> for Object {
     fn from(b: bool) -> Self {
@@ -137,6 +83,49 @@ impl From<&ParserError> for Object {
         Object::Error {
             message: value.kind.to_string(),
             span: value.span,
+        }
+    }
+}
+
+impl Hash for Object {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        match self {
+            Object::Int(i) => i.hash(state),
+            Object::Float(f) => f.to_bits().hash(state),
+            Object::Str(s) => s.hash(state),
+            Object::Bool(b) => b.hash(state),
+            Object::List { data, .. } => data.hash(state),
+            Object::Map { .. } => "map".hash(state),
+            Object::Fn { name, .. } => name.hash(state),
+            Object::Nil => {}
+            Object::Return(v) => v.hash(state),
+            Object::Error { message, span } => {
+                message.hash(state);
+                span.hash(state);
+            }
+        }
+    }
+}
+
+impl fmt::Display for Object {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Object::Int(i) => write!(f, "{i}"),
+            Object::Float(x) => write!(f, "{x:?}"),
+            Object::Str(s) => write!(f, "{s}"),
+            Object::Bool(b) => write!(f, "{b}"),
+            Object::List { data, .. } => write!(f, "{data:?}"),
+            Object::Map { data, .. } => write!(f, "{data:?}"),
+            Object::Fn { .. } => write!(f, "Fn[{}]", self.calculate_hash()),
+            Object::Nil => write!(f, "nil"),
+            Object::Return(v) => write!(f, "{v}"),
+            Object::Error {
+                message,
+                span: trace,
+            } => {
+                let ((l1, c1), (l2, c2)) = trace;
+                write!(f, "{l1}:{c1}-{l2}:{c2} {}", message)
+            }
         }
     }
 }
