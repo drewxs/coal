e3d30cb74e8a98ba2e305c214cf51c28ebc5e46f
Andrew X. Shah <drew@drewxs.dev>
2024-12-17 14:32:52 -0700

    test(evaluator): add scope/invalid assignment tests

--- coal-core/src/evaluator/tests.rs
+++ coal-core/src/evaluator/tests.rs
@@ -1,3 +1,5 @@
+use std::assert_matches::assert_matches;
+
 use test::Bencher;
 
 use crate::{Expr, Ident, Infix, Stmt, Type, Var};
@@ -5,30 +7,12 @@ use crate::{Expr, Ident, Infix, Stmt, Type, Var};
 use super::{Evaluator, Object, FALSE, TRUE};
 
 #[test]
-fn test_eval_int_expressions() {
+fn test_eval_literal() {
     let tests = vec![
         ("5", Some(Object::Int(5))),
-        ("10", Some(Object::Int(10))),
-        ("-5", Some(Object::Int(-5))),
-        ("-10", Some(Object::Int(-10))),
-    ];
-
-    let mut evaluator = Evaluator::default();
-
-    for (expr, expected) in tests {
-        assert_eq!(expected, evaluator.eval(expr));
-    }
-}
-
-#[test]
-fn test_eval_not_expressions() {
-    let tests = vec![
-        ("!true", Some(FALSE)),
-        ("!!true", Some(TRUE)),
-        ("!false", Some(TRUE)),
-        ("!!false", Some(FALSE)),
-        ("!5", Some(FALSE)),
-        ("!!5", Some(TRUE)),
+        ("3.14", Some(Object::Float(3.14))),
+        ("true", Some(TRUE)),
+        (r#""foo""#, Some(Object::Str(String::from("foo")))),
     ];
 
     let mut evaluator = Evaluator::default();
@@ -59,7 +43,27 @@ fn test_eval_str_interpolation() {
 }
 
 #[test]
-fn test_eval_infix_expressions() {
+fn test_eval_prefix() {
+    let tests = vec![
+        ("-5", Some(Object::Int(-5))),
+        ("-10", Some(Object::Int(-10))),
+        ("!true", Some(FALSE)),
+        ("!!true", Some(TRUE)),
+        ("!false", Some(TRUE)),
+        ("!!false", Some(FALSE)),
+        ("!5", Some(FALSE)),
+        ("!!5", Some(TRUE)),
+    ];
+
+    let mut evaluator = Evaluator::default();
+
+    for (expr, expected) in tests {
+        assert_eq!(expected, evaluator.eval(expr));
+    }
+}
+
+#[test]
+fn test_eval_infix() {
     let tests = vec![
         ("(7 + 2 * 3 / 2) % 3", Some(Object::Float(1.0))),
         ("1 + 2 * 3 + 4 / 5", Some(Object::Float(7.8))),
@@ -103,7 +107,7 @@ fn test_eval_infix_expressions() {
 }
 
 #[test]
-fn test_eval_if_expressions() {
+fn test_eval_if() {
     let tests = vec![
         ("if true { 10 }", Some(Object::Int(10))),
         ("if false { 10 }", None),
@@ -140,7 +144,7 @@ fn test_eval_if_expressions() {
 }
 
 #[test]
-fn test_eval_nested_if_expressions() {
+fn test_eval_nested_if() {
     let tests = vec![
         (
             r#"if true {
@@ -174,7 +178,7 @@ fn test_eval_nested_if_expressions() {
 }
 
 #[test]
-fn test_eval_let_statements() {
+fn test_eval_let() {
     let tests = vec![
         ("let x = 7; x;", Some(Object::Int(7))),
         ("let x = 2 * 3; x;", Some(Object::Int(6))),
@@ -184,13 +188,6 @@ fn test_eval_let_statements() {
             "let x = 7; let y = 10; let z: int = x + y + 3; z;",
             Some(Object::Int(20)),
         ),
-        (
-            "foo;",
-            Some(Object::Error {
-                message: String::from("identifier not found: foo"),
-                span: ((1, 1), (1, 3)),
-            }),
-        ),
     ];
 
     for (expr, expected) in tests {
@@ -199,23 +196,22 @@ fn test_eval_let_statements() {
 }
 
 #[test]
-fn test_eval_assign_statements() {
+fn test_eval_let_existing() {
+    let input = "fn x() -> int {}; let x = 1;";
+    assert_matches!(Evaluator::default().eval(input), Some(Object::Error { .. }));
+}
+
+#[test]
+fn test_eval_let_scope() {
+    let input = "fn x() -> int {}; let x = 1;";
+    assert_matches!(Evaluator::default().eval(input), Some(Object::Error { .. }));
+}
+
+#[test]
+fn test_eval_assign() {
     let tests = vec![
         ("let x = 1; x = 2; x;", Some(Object::Int(2))),
-        (
-            "x = 1",
-            Some(Object::Error {
-                message: String::from("identifier not found: x"),
-                span: ((1, 1), (1, 1)),
-            }),
-        ),
-        (
-            r#"let x = 1; x = "foo""#,
-            Some(Object::Error {
-                message: String::from("type mismatch: expected=int, got=str"),
-                span: ((1, 16), (1, 20)),
-            }),
-        ),
+        ("let x = 1; x = x + x + x; x;", Some(Object::Int(3))),
     ];
 
     for (expr, expected) in tests {
@@ -224,7 +220,20 @@ fn test_eval_assign_statements() {
 }
 
 #[test]
-fn test_eval_fn_expr() {
+fn test_eval_assign_invalid() {
+    let tests = vec![
+        "x = 1",
+        r#"let x = 1; x = "foo";"#,
+        "fn x() -> int {}; x = 1;",
+        "fn x() -> int {}; let x = 1;",
+    ];
+    for input in tests {
+        assert_matches!(Evaluator::default().eval(input), Some(Object::Error { .. }));
+    }
+}
+
+#[test]
+fn test_eval_function() {
     let tests = vec![(
         r#"fn add(x: int, y: int) -> int {
             if x > y {
@@ -269,7 +278,7 @@ fn test_eval_fn_expr() {
 }
 
 #[bench]
-fn bench_eval_math_expression(b: &mut Bencher) {
+fn bench_eval_math(b: &mut Bencher) {
     let mut evaluator = Evaluator::default();
     let test =
         "(((9876 * 5432) // 123 + (8765 % 34) * (4321 // 2)) * 1987) % 567 + (3456 * 7890) // 234";
