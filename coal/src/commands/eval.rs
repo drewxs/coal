7ec2f7cd3f71fd41fae423dfb5c7b00a7ed57e22
Andrew X. Shah <drew@drewxs.dev>
2024-12-21 22:10:49 -0700

    refactor(cli): cleanup

--- coal/src/commands/eval.rs
+++ coal/src/commands/eval.rs
@@ -5,31 +5,30 @@ pub fn eval(input: &str) {
 }
 
 pub fn eval_with(evaluator: &mut Evaluator, input: &str) {
-    if let Some(obj) = evaluator.eval(input) {
-        match obj {
-            Object::Error(e) => {
-                let ((l1, c1), (l2, c2)) = e.span;
+    match evaluator.eval(input) {
+        Some(Object::Error(e)) => {
+            let ((l1, c1), (l2, c2)) = e.span;
 
-                if input.lines().count() > 1 {
-                    let line = input.lines().nth(l1 - 1).unwrap();
-                    println!("\x1b[31m{}\x1b[0m", "-".repeat(75));
-                    println!("{}", clean_input(line));
-                    println!(
-                        "\x1b[31m{}\x1b[0m",
-                        " ".repeat(c1 - 1) + &"^".repeat(c2 - c1 + 1),
-                    );
-                    println!("{l1}:{c1}-{l2}:{c2} {}\n", e.kind);
-                } else {
-                    println!("\x1b[31m{}\x1b[0m", "-".repeat(75));
-                    println!("{}", clean_input(input));
-                    println!(
-                        "\x1b[31m{}\x1b[0m",
-                        " ".repeat(c1 - 1) + &"^".repeat(c2 - c1 + 1),
-                    );
-                    println!("{}\n", e.kind);
-                }
+            if input.lines().count() > 1 {
+                let line = input.lines().nth(l1 - 1).unwrap();
+                println!("\x1b[31m{}\x1b[0m", "-".repeat(75));
+                println!("{}", clean_input(line));
+                println!(
+                    "\x1b[31m{}\x1b[0m",
+                    " ".repeat(c1 - 1) + &"^".repeat(c2 - c1 + 1),
+                );
+                println!("{l1}:{c1}-{l2}:{c2} {}\n", e.kind);
+            } else {
+                println!("\x1b[31m{}\x1b[0m", "-".repeat(75));
+                println!("{}", clean_input(input));
+                println!(
+                    "\x1b[31m{}\x1b[0m",
+                    " ".repeat(c1 - 1) + &"^".repeat(c2 - c1 + 1),
+                );
+                println!("{}\n", e.kind);
             }
-            _ => println!("{obj}"),
         }
+        Some(obj) => println!("{obj}"),
+        _ => {}
     }
 }
