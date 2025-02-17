3a6d13aaf352574dfc188be754334912d5787873
Krishna Chittur <krishna@chittur.dev>
2025-02-16 20:45:15 -0500

    chore: sort parser errors

--- coal-core/src/parser/error.rs
+++ coal-core/src/parser/error.rs
@@ -23,65 +23,26 @@ impl fmt::Display for ParserError {
 
 #[derive(Clone, Debug)]
 pub enum ParserErrorKind {
-    SyntaxError(TokenKind),
-    UnexpectedToken(TokenKind, TokenKind),
-    NotFound(String),
-    MethodNotFound(Type, String),
-    TypeAnnotationsNeeded,
-    InvalidArgumentsLength(usize, usize),
-    TypeMismatch(Type, Type),
-    NonIndexableType(Type),
-    InvalidIndex(Type, Type),
-    MissingElseClause,
-    UnhashableMapKey(Type),
-    InvalidStructAttr(String),
     AttrMissing(String),
     DuplicateAttr(String),
     DuplicateFunc(String),
+    InvalidArgumentsLength(usize, usize),
+    InvalidIndex(Type, Type),
+    InvalidStructAttr(String),
+    MethodNotFound(Type, String),
+    MissingElseClause,
+    NotFound(String),
+    NonIndexableType(Type),
+    SyntaxError(TokenKind),
+    TypeAnnotationsNeeded,
+    TypeMismatch(Type, Type),
+    UnexpectedToken(TokenKind, TokenKind),
+    UnhashableMapKey(Type),
 }
 
 impl fmt::Display for ParserErrorKind {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            ParserErrorKind::SyntaxError(token) => {
-                write!(f, "syntax error: `{token}`")
-            }
-            ParserErrorKind::UnexpectedToken(t1, t2) => {
-                write!(f, "unexpected token: `{t1}`, expected: `{t2}`")
-            }
-            ParserErrorKind::NotFound(name) => {
-                write!(f, "not found: `{name}`")
-            }
-            ParserErrorKind::MethodNotFound(t, name) => {
-                write!(f, "method not found: `{t}.{name}()`")
-            }
-            ParserErrorKind::TypeAnnotationsNeeded => {
-                write!(f, "type annotations needed")
-            }
-            ParserErrorKind::InvalidArgumentsLength(n1, n2) => {
-                write!(
-                    f,
-                    "invalid number of arguments. expected: `{n1}`, got: `{n2}`"
-                )
-            }
-            ParserErrorKind::TypeMismatch(t1, t2) => {
-                write!(f, "type mismatch. expected: `{t1}`, got: `{t2}`")
-            }
-            ParserErrorKind::NonIndexableType(t) => {
-                write!(f, "type `{t}` is not indexable")
-            }
-            ParserErrorKind::InvalidIndex(t, idx_t) => {
-                write!(f, "the type `{t}` cannot be indexed by `{idx_t}`")
-            }
-            ParserErrorKind::MissingElseClause => {
-                write!(f, "`if` may be missing an `else` clause")
-            }
-            ParserErrorKind::UnhashableMapKey(t) => {
-                write!(f, "map key not hashable: type `{t}`")
-            }
-            ParserErrorKind::InvalidStructAttr(name) => {
-                write!(f, "invalid struct attribute: `{name}`")
-            }
             ParserErrorKind::AttrMissing(name) => {
                 write!(f, "missing required attribute: `{name}`")
             }
@@ -90,6 +51,45 @@ impl fmt::Display for ParserErrorKind {
             }
             ParserErrorKind::DuplicateFunc(name) => {
                 write!(f, "function already defined: `{name}`")
+            }
+            ParserErrorKind::InvalidArgumentsLength(n1, n2) => {
+                write!(
+                    f,
+                    "invalid number of arguments. expected: `{n1}`, got: `{n2}`"
+                )
+            }
+            ParserErrorKind::InvalidIndex(t, idx_t) => {
+                write!(f, "the type `{t}` cannot be indexed by `{idx_t}`")
+            }
+            ParserErrorKind::InvalidStructAttr(name) => {
+                write!(f, "invalid struct attribute: `{name}`")
+            }
+            ParserErrorKind::MethodNotFound(t, name) => {
+                write!(f, "method not found: `{t}.{name}()`")
+            }
+            ParserErrorKind::MissingElseClause => {
+                write!(f, "`if` may be missing an `else` clause")
+            }
+            ParserErrorKind::NonIndexableType(t) => {
+                write!(f, "type `{t}` is not indexable")
+            }
+            ParserErrorKind::NotFound(name) => {
+                write!(f, "not found: `{name}`")
+            }
+            ParserErrorKind::SyntaxError(token) => {
+                write!(f, "syntax error: `{token}`")
+            }
+            ParserErrorKind::TypeAnnotationsNeeded => {
+                write!(f, "type annotations needed")
+            }
+            ParserErrorKind::TypeMismatch(t1, t2) => {
+                write!(f, "type mismatch. expected: `{t1}`, got: `{t2}`")
+            }
+            ParserErrorKind::UnexpectedToken(t1, t2) => {
+                write!(f, "unexpected token: `{t1}`, expected: `{t2}`")
+            }
+            ParserErrorKind::UnhashableMapKey(t) => {
+                write!(f, "map key not hashable: type `{t}`")
             }
         }
     }
