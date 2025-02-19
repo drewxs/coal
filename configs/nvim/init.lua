2bbee15bb1c759641655514499ca63f49c0b0cc6
Andrew X. Shah <drew@drewxs.dev>
2025-02-18 22:05:53 -0700

    chore(configs/nvim): rm extra opts

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -1,21 +1,14 @@
+local lazyrepo = "https://github.com/folke/lazy.nvim.git"
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
   local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
   if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
     vim.fn.getchar()
     os.exit(1)
   end
 end
 vim.opt.rtp:prepend(lazypath)
 
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 
@@ -37,11 +30,7 @@ vim.treesitter.language.register("rust", "coal")
 
 require("conform").setup({
   formatters = {
-    coal_fmt = {
-      command = "coal",
-      args = { "fmt" },
-      stdin = true,
-    },
+    coal_fmt = { command = "coal", args = { "fmt" }, stdin = true },
   },
   formatters_by_ft = {
     coal = { "coal_fmt" },
