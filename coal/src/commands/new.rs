223d602fe6cacfbc03a0ddf8f76c79f5d4743433
Andrew X. Shah <drew@drewxs.dev>
2026-04-24 20:29:11 -0600

    fix(cli/new): init git, readme

--- coal/src/commands/new.rs
+++ coal/src/commands/new.rs
@@ -1,10 +1,14 @@
-use std::{fs, io};
+use std::{fs, io, process::Command};
 
 const HELLO_TEMPLATE: &str = include_str!("../../templates/hello.coal");
 
 pub fn new(name: &str) -> io::Result<String> {
     fs::create_dir_all(format!("{name}/src"))?;
     fs::write(format!("{name}/src/main.coal"), HELLO_TEMPLATE)?;
+    fs::write(format!("{name}/README.md"), format!("# {name}\n"))?;
+    fs::write(format!("{name}/.gitignore"), "/target\n")?;
+
+    Command::new("git").arg("init").arg(name).output()?;
 
     Ok(format!("Project created `{name}`"))
 }
